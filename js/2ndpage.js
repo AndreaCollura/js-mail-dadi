0ca60ff70b2ce8f532a0e2fd8121a161322077cd
AndreaCollura <andrea.collur@gmail.com>
2023-03-13 21:42:22 +0100

    added css test + javescript dice test

--- js/2ndpage.js
+++ js/2ndpage.js
@@ -1,36 +1,96 @@
 
+        
+        
+        
+        
+        
+        
+        
 
+
+
+
+const diceNumUser = getRndNumber (1,6);
+const diceNumPc = getRndNumber (1,6);
+const player = document.getElementById('player');
+const pc = document.getElementById('pc');
+const whoWin = document.getElementById('results');
 const btnRoll = document.getElementById('diceroll');
+
+
+
 btnRoll.addEventListener('click', rollDice );
 
 
-let diceNumUser = getRndNumber (1,6);
-let diceNumPc = getRndNumber (1,6);
+
+
+const yourResult = `
+     
+    <h2>you</h2>
+    <h1> ${diceNumUser}</h1>
+     
+     
+`;
+
+const pcResult = `
+
+    <h2>pc</h2>
+    <h1> ${diceNumPc}</h1>
+
+
+`;        
+
 
 
 
 function rollDice (){
-    const diceNumUser = getRndNumber (1,6);
-    const diceNumPc = getRndNumber (1,6);
-
+    
+    
+    whoWin.classList.remove('opacity-0');
+    player.innerHTML = yourResult;
+    pc.innerHTML = pcResult;
+    
+    
    if(diceNumUser > diceNumPc) {
-    console.log('hai vinto!')
+
+    verdict.innerHTML = `
+        <h1 class="text-uppercase">you win!!</h1>
+
+    
+    `;
+   
+   
         
         
     } else if (diceNumUser === diceNumPc){
-        console.log('pareggio!')
+    
+    
+   
+
+    verdict.innerHTML = `
+        <h1 class="text-uppercase">tie!!</h1>
+    `;
         
-       
+    
 
     } else {
-        console.log('hai perso!')
         
+    verdict.innerHTML = `
+        <h1 class="text-uppercase">you lose!!</h1>
+    `;
+
+ 
       
     }
 
+    
 
 }
 
+
+
+
+console.log(verdict,pcResult,yourResult);
 
 
 
